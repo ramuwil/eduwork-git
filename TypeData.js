dc970db887e75297a55b9c7034c3df66458d3b0b
ramuwil <ramuwil@gmail.com>
2024-01-18 09:53:24 +0700

    menambahkan file typedata.js

---
+++ TypeData.js
@@ -0,0 +1,7 @@
+let length = 16;
+let lastname ="jhonson";
+const x ={  fristname:"Jhon",
+            lastname:"Doe"};
+console.log(typeof length);
+console.log(typeof lastname);
+console.log(typeof x);
