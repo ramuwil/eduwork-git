bb1fc82e5f9ff2471508d2881dfd7b58a9189d25
ramuwil <ramuwil@gmail.com>
2024-02-02 12:51:10 +0700

    modifikasi file search.js

--- edubdd/search.js
+++ edubdd/search.js
@@ -1,12 +1,17 @@
+import searchPage from "./searchPage";
 const {Given, When, Then}= require ("@badeball/cypress-cucumber-preprocessor");
 
 Given('I visited the website',() =>{
-    cy.visit('http://zero.webappsecurity.com/index.html');
-    cy.url().should('include','index.html')
+    searchPage.visit()
+    //cy.visit('http://zero.webappsecurity.com/index.html');
+    //cy.url().should('include','index.html')
 })
 When('I did a search on the search box menu', () =>{
-    cy.get('#searchTerm').type('banking {enter}')
+    searchPage.searching()
+    //cy.get('#searchTerm').type('banking {enter}')
 })
 Then('I succeeded in searching',() =>{
-    cy.get('.brand').should('be.visible')
+    searchPage.successed()
+    //cy.get('.brand').should('be.visible')
 })
+
